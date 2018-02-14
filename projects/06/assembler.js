87cf8ddf3c0e5294349e754bf84a258bdda8063c
Reed Schimmel <reed.schimmel@kingfit.io>
2018-02-13 21:15:21 -0800

    Project 6 complete

--- projects/06/assembler.js
+++ projects/06/assembler.js
@@ -1,10 +1,13 @@
-const argv = require('yargs').argv;
+/** Program Flow
+ * node assembler.js ****.asm
+ * 
+*/
 
-if (argv.ships > 3 && argv.distance < 53.5) {
-  console.log('Plunder more riffiwobbles!');
-} else {
-  console.log('Retreat from the xupptumblers!');
-}
+// IMPORTS
+const fs = require('fs');
+
+// const asmFile = './add/Add.asm'; //TODO
+const asmFile = process.argv[2]; //TODO
 
 const INIT_SYMBOL_TABLET = {
   'R0': 0,
@@ -50,11 +53,9 @@ const COMP_TABLE ={
   'D-X': '010011',
   'X-D': '000111',
   'D&X': '000000',
-  'D|A': '010101'
+  'D|X': '010101'
 };
 
-// const DEST_TABLE ={};
-//
 const JUMP_TABLE ={
   'JGT': '001',
   'JEQ': '010',
@@ -65,55 +66,221 @@ const JUMP_TABLE ={
   'JMP': '111'
 };
 
-const comp = compString => {
-  // I do not think there is never a null case
-  // if (compString.includes('A'))
-  const a = Number(compString.includes('A')).toString();
-  const key = compString.replace('A','X').replace('M','X');
-  return `${a}${COMP_TABLE[key]}`;
+const assemble = (asmFile, symbols) => {
+  const comp = compString => {
+    // console.log('compString', compString);
+    // If compString contains an 'A': a = 0
+    const key = removeSpaces(compString.replace('A','X').replace('M','X'));
+    return `${COMP_TABLE[key]}`;
+    // const a = Number(!compString.includes('A')).toString();
+    // const key = removeSpaces(compString.replace('A','X').replace('M','X'));
+    // return `${a}${COMP_TABLE[key]}`;
+  };
+  
+  const dest = destString => {
+    const m = Number(destString.includes('M'));
+    const a = Number(destString.includes('A'));
+    const d = Number(destString.includes('D'));
+    return `${a}${d}${m}`;
+  };
+  
+  const jump = jumpString => {
+    // console.log('jumpString', jumpString);
+    if (!jumpString) return '000';
+    return JUMP_TABLE[jumpString];
+  };
+
+  // first we define functions
+  const aInstruction = instruction => {
+    // code could be a decimal number or variable/symbole
+    const value = symbols[instruction];
+    // console.log('A instruction', instruction, value);
+    return (value >= 0 ? decToBin(value) : decToBin(instruction));
+    // if (value) {
+    //   return value;
+    // }
+    // return decToBin(instruction);
+  };
+  
+  const cInstruction = instruction => {
+    // console.log('C instruction', instruction);
+    //dest=comp;jump
+    const doesJump = instruction.includes(';J');
+    // console.log('we jump', doesJump);
+    const doesCompute = instruction.includes('=');
+    const compEnd = (doesJump ? instruction.indexOf(';') : instruction.length);
+
+    let dst='000';
+    let cmp;
+    let jmp='000';
+    let a='0';
+
+    if (doesCompute) {
+      const eIndex = instruction.indexOf('=');
+
+      dst = dest(instruction.slice(0, eIndex));
+      cmp = comp(instruction.slice(eIndex + 1, compEnd));
+      if (instruction.slice(eIndex + 1, compEnd).includes('A')) {
+        a = 0;
+      } else if (instruction.slice(eIndex + 1, compEnd).includes('M')) {
+        a = 1;
+      }
+      // a = Number(!instruction.slice(eIndex + 1, compEnd).includes('A')).toString();
+      // a = Number(!instruction.slice(eIndex + 1, compEnd).includes('A')).toString();
+      // console.log('a bit',a) 
+      
+    } else {
+      //D;JMP
+      cmp = comp(instruction.slice(0, compEnd));
+      a='0';
+      // console.log('instruction: ', instruction);
+      // console.log(instruction.slice(0, compEnd),'comp code: ', cmp);
+    }
+
+    if(doesJump) {
+      jmp = jump(instruction.slice(compEnd + 1), instruction.length);
+    }
+    // C-instruction: 111a c1c2c3c4 c5c6d1d2 d3j1j2j3
+    return `111${a}${cmp}${dst}${jmp}`;
+  };
+
+  const parse = line => {
+    // console.log(line);
+    if (line.startsWith('@')) return aInstruction(line.slice(1, line.length + 1));
+    else if (!line.startsWith('(')) return cInstruction(line);
+  };
+
+  // time to assemble
+  let compiled = '';
+  for (let line of asmFile) {
+    const compLine = parse(line);
+    try {
+      if (compLine.includes('undefined')) {
+        console.log(line,compLine);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+    if (compLine !== undefined) compiled += `${compLine}\n`;
+  }
+  return compiled;
 };
 
-const dest = destString => {
-  const m = Number(destString.includes('M'));
-  const a = Number(destString.includes('A'));
-  const d = Number(destString.includes('D'));
-  return `${a}${d}${m}`;
-};
-
-const jump = jumpString => {
-  if (!jumpString) return '000';
-  return JUMP_TABLE[jumpString];
-};
-
-const aInstruction = code => {
-
-};
-
-const cInstruction = code => {
-
-};
-
-const parse = code => {
-  if (code.startsWith('@')) aInstruction(code);
-  else cInstruction(code);
+const writeToFile = hackProgram => {
+  // console.log(hackProgram);
+  const filename = asmFile.slice(0, asmFile.length-3) + 'hack';
+  fs.writeFileSync(filename, hackProgram, 'utf8');
 };
 
 const initialization = asmFile => {
-
+  const labelSymbols = firstPass(asmFile, INIT_SYMBOL_TABLET);
+  const symbols = secondPass(asmFile, labelSymbols);
+  const hackProgram = assemble(asmFile, symbols);
+  writeToFile(hackProgram);
 };
 
 const firstPass = (asmFile, symbolTable) => {
   // extract label symbols
+  const extractLabel = statement => {
+    const start = statement.indexOf('(');
+    const end = statement.indexOf(')');
+    if ( start >= 0 && end > 0) {
+      return statement.slice(start + 1, end);
+    } else if (start === end) return null;
+  };
+
+  // for (let statement of asmFile) {
+  let realLineNumber = 0;
+  for (let statement of asmFile) {
+    //(LOOP) we must extract LOOP
+    let label = extractLabel(statement);
+    if (label) {
+      symbolTable[label] = realLineNumber;
+      // symbolTable[label] = realLineNumber + 1;
+    } else {
+      realLineNumber++;
+    }
+  }
+  // for (let lineNum = 0; lineNum < asmFile.length ; lineNum++) {
+  //   //(LOOP) we must extract LOOP
+  //   statement = asmFile[lineNum];
+  //   let label = extractLabel(statement);
+  //   if (label) {
+  //     symbolTable[label] = realLineNumber + 1;
+  //   } else if (label === -2) realLineNumber++;
+  // }
   return symbolTable;
 };
 
 const secondPass = (asmFile, symbolTable) => {
   // extract varable symbols
+  const extractVariable = statement => {
+    const isAt = statement.startsWith('@');
+    const literalNumber = statement.slice(1,statement.length) >= 0;
+    if (isAt && !literalNumber) {
+      const variable = statement.slice(1, statement.length);
+      return variable;
+    }
+  };
+
+  let ramCounter = 16;
+  let statement;
+  for (let lineNum = 0; lineNum < asmFile.length ; lineNum++) {
+    statement = asmFile[lineNum];
+    const variable = extractVariable(statement);
+    symbolTable[variable];
+    if (variable && symbolTable[variable] === undefined) {
+      symbolTable[variable] = ramCounter++;
+      // symbolTable[variable] = lineNum;
+    }
+  }
   return symbolTable;
 };
 
-const decToBin = dec => Number(dec.toString(2));
+const decToBin = dec =>{
+  const pad = (s, size) => {    
+    const padding = '0000000000000000';
+    const amountOfPadding = size - s.toString().length;
+    const slicedPadding = padding.slice(0,amountOfPadding);
+    return `${slicedPadding}${s}`;
+  };
+  return pad(Number(dec).toString(2), 16);
+};
 
 const removeSpaces = string => {
-
+  let newString = string.trim();
+  while (newString.includes(' ')) {
+    newString = newString.replace(' ', '');
+  }
+  return newString;
 };
+
+const parseFile = fileData => {
+  // returns an array of each line of file
+  let character;
+  let line;
+  const array = [];
+  for (character of fileData) {
+    if (character === '\n') {
+      if (!line.startsWith('//') && line !== '\r' && !line.includes('undefined')) {
+        line = line.replace('\r', '');
+        const end = line.indexOf('//');
+        if (end !== -1) line = line.slice(0, end);
+        array.push(removeSpaces(line));
+      }
+      line = '';
+    } else {
+      line += character;
+    }
+  }
+  // console.log(fileData)
+  return array;
+};
+
+try {  
+  const data = fs.readFileSync(asmFile, 'utf8');
+  const instructions = parseFile(data);
+  initialization(instructions);
+} catch(e) {
+  console.log('Error:', e.stack);
+}
